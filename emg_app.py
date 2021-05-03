2b29b8bbb91bf94c42ebd2738f69a336dd71395f
ldlasso12 <laurad.lasso.a@gmail.com>
2021-05-02 20:10:40 -0500

    Update code for plot 4 movements

--- emg_app.py
+++ emg_app.py
@@ -1,52 +1,48 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-
-st.title('Welcomo to the Streamlit of Trascender Global')
+st.title('IBM Cloud Data Challenge - Hand Movement Prediction using EMG')
+st.write('Welcomo to the Streamlit of Trascender Global')
 st.write("Hello there! Are you ready to test our model for predict a human movement, this model was train in IBM-AutoAI. You can read our blog post, where we describe all process to train it.")
 
 
 #%% PLOT Signal
 st.write("Remember, this is a EMG signal.")
 
-Sx = open("D:\OneDrive - Trascender Global\EMG-project\movimientos\Movimiento4AV.txt", "r").read()
-lines2 = Sx.split('\n')
+def read_txt(name):
+    Sx = open(name, "r").read()
+    sx1 = [] #Un sensor
+    # Ciclo while para leer los datos del txt, donde se aseguran que se reciban los 4 datos de la señal,
+    # sino es asi se salta el dato erroneo
+    i = 0
+    lines2 = Sx.split('\n')
+    while i < len(lines2):
+        if i == len(lines2):
+            i = len(lines2) * 100
+            break
+        da = lines2[i]
 
-sx1 = []
-sx2 = []
-sx3 = []
-sx4 = []
+        if len(da)>7:
+            d1, d2, d3, d4 = da.split(",")
 
-# Ciclo while para leer los datos del txt, donde se aseguran que se reciban los 4 datos de la señal,
-# sino es asi se salta el dato erroneo
-i = 0
-while i < len(lines2):
-    if i == len(lines2):
-        i = len(lines2) * 100
-        break
-    da = lines2[i]
-
-    if len(da)>7:
-        d1, d2, d3, d4 = da.split(",")
-
-        if (len(d1)>0) and (len(d2)>0) and (len(d3)>0) and (len(d4)>0):
-            sx1.append(d1)
-            sx2.append(d2)
-            sx3.append(d3)
-            sx4.append(d4)
+            if (len(d1)>0) and (len(d2)>0) and (len(d3)>0) and (len(d4)>0):
+                sx1.append(d1)
+                i = i + 1
+        else:
             i = i + 1
-    else:
-        i = i + 1
-# Variables que son transformadas de listas a array para realizar procesos matematicos
-SX1 = np.array(sx1).astype(np.float)  # Señal del canal1
-SX2 = np.array(sx2).astype(np.float)  # Señal del canal 2
-SX3 = np.array(sx3).astype(np.float)  # Señal del canal 3
-SX4 = np.array(sx4).astype(np.float)  # Señal del canal 4
+    # Variables que son transformadas de listas a array para realizar procesos matematicos
+    SX1 = np.array(sx1).astype(np.float)  # Señal del canal1
+    
+    return SX1
+    
+SX1 = read_txt('movimientos\Movimiento1AV.txt')
+SX2 = read_txt('movimientos\Movimiento2AV.txt')
+SX3 = read_txt('movimientos\Movimiento3AV.txt')
+SX4 = read_txt('movimientos\Movimiento4AV.txt')
 
-chart_data = pd.DataFrame(     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
+chart_data = pd.DataFrame(list(zip(SX1, SX2,SX3, SX4)),columns=['Movement 1', 'Movement 2', 'Movement 3', 'Movement 4'])
 
-st.line_chart(SX1)
+st.line_chart(chart_data)
 
 #%% PREDICTION
 st.write("Now, predict with the csv file. You can choose a trascender signal and predict what movement was doing!")
